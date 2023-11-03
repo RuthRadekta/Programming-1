417699d76236b465bba9731e116154f70d62bf77
Refa Hilyah <refahilyaa@student.uns.ac.id>
2023-11-03 09:22:32 +0700

    Update Perpusdata.java
    Add data

--- perpusdata/src/main/java/com/mycompany/perpusdata/Perpusdata.java
+++ perpusdata/src/main/java/com/mycompany/perpusdata/Perpusdata.java
@@ -4,13 +4,33 @@
 
 package com.mycompany.perpusdata;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
 /**
  *
  * @author ASUS
  */
 public class Perpusdata {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws SQLException {
         System.out.println("Hello Dita!");
+        
+        Koneksi konek = new Koneksi();
+        Connection koneksi = konek.buka();
+        
+        try {
+            String query = "insert into anggota (id_anggota, nama, jenis_kelamin, alamat, email, created_at) values (?, ?, ?, ?, ?, ?)";
+            PreparedStatement ps = koneksi.prepareStatement(query);
+            ps.setString(1, "Luffy");
+            ps.setString(2, "One Piece");
+            ps.setString(3, "Luffy");
+            ps.setString(4, "One Piece");
+            ps.addBatch();
+            int[] jml = ps.executeBatch();
+        } catch (SQLException ex) {
+        System.out.println(ex.getMessage());  
+        }
     }
 }
