7dbfe75c8c74c052e0fd0503c3a8db5ff1fcdefb
Refa Hilyah <refahilyaa@student.uns.ac.id>
2023-11-03 09:10:12 +0700

    Update Perpusdata.java
    Hello Dita!

--- perpusdata/src/main/java/com/mycompany/perpusdata/Perpusdata.java
+++ perpusdata/src/main/java/com/mycompany/perpusdata/Perpusdata.java
@@ -11,6 +11,6 @@ package com.mycompany.perpusdata;
 public class Perpusdata {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Hello Dita!");
     }
 }
