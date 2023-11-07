57a1d63985e0068a2aa7a97e04596bf4fbf1c61d
Refa Hilyah <refahilyaa@student.uns.ac.id>
2023-11-07 18:39:52 +0700

    guak main

--- perpusdata/src/main/java/com/mycompany/perpusdata/anggotaread.java
+++ perpusdata/src/main/java/com/mycompany/perpusdata/anggotaread.java
@@ -153,7 +153,7 @@ public class anggotaread extends javax.swing.JFrame {
     /**
      * @param args the command line arguments
      */
-    public static void main(String args[]) {
+    public static void anggotaread(String args[]) {
         /* Set the Nimbus look and feel */
         //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
         /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
@@ -180,7 +180,7 @@ public class anggotaread extends javax.swing.JFrame {
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
-                new anggotaread().setVisible(false);
+                new anggotaread().setVisible(true);
             }
         });
     }
