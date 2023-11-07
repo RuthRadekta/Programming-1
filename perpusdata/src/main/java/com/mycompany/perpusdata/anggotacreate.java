775e347be4fedc18c8a81af036dbda8d4943aa5e
Refa Hilyah <refahilyaa@student.uns.ac.id>
2023-11-07 18:38:09 +0700

    guak main

--- perpusdata/src/main/java/com/mycompany/perpusdata/anggotacreate.java
+++ perpusdata/src/main/java/com/mycompany/perpusdata/anggotacreate.java
@@ -220,7 +220,7 @@ public class anggotacreate extends javax.swing.JFrame {
     /**
      * @param args the command line arguments
      */
-    public static void main(String args[]) {
+    public static void anggotacreate(String args[]) {
         /* Set the Nimbus look and feel */
         //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
         /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
@@ -247,7 +247,7 @@ public class anggotacreate extends javax.swing.JFrame {
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
-                new anggotacreate().setVisible(false);
+                new anggotacreate().setVisible(true);
             }
         });
     }
--- perpusdata/src/main/java/com/mycompany/perpusdata/anggotadelete.java
+++ perpusdata/src/main/java/com/mycompany/perpusdata/anggotadelete.java
@@ -283,7 +283,7 @@ public class anggotadelete extends javax.swing.JFrame {
     /**
      * @param args the command line arguments
      */
-    public static void main(String args[]) {
+    public static void anggotadelete(String args[]) {
         /* Set the Nimbus look and feel */
         //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
         /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
@@ -310,7 +310,7 @@ public class anggotadelete extends javax.swing.JFrame {
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
-                new anggotadelete().setVisible(false);
+                new anggotadelete().setVisible(true);
             }
         });
     }
