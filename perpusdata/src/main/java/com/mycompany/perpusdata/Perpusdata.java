f004e8d7f0809f6b2f99d0e8ffef4bb0c43e9c1c
Refa Hilyah <refahilyaa@student.uns.ac.id>
2023-11-13 00:39:00 +0700

    nutup koneksi kalo semua proses udah selese

--- perpusdata/src/main/java/com/mycompany/perpusdata/Perpusdata.java
+++ perpusdata/src/main/java/com/mycompany/perpusdata/Perpusdata.java
@@ -4,15 +4,19 @@
 
 package com.mycompany.perpusdata;
 
+import java.sql.SQLException;
+
 
 /**
  *
  * @author ASUS
  */
 public class Perpusdata {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws SQLException {
         System.out.println("Hello Dita!");
         loginform login = new loginform();
         login.setVisible(true);
+        Koneksi koneksi = new Koneksi();
+        koneksi.tutup();
     }
 }
